bf347bdf9f859d60251d09953e48f01a1b6ed6dd
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2018-06-17 16:57:52 +0900

    test

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,8 +1,10 @@
+ls
+
 if [ "$TRAVIS_OS_NAME" = "osx" ] ; then
-    bash -x ../build_macos_binary.sh
+    bash -x build_macos_binary.sh
 elif [ "$TRAVIS_OS_NAME" = "linux" ] ; then
     sudo apt -qq update
     sudo apt install -y fakeroot
 
-    bash -x ../build_deb_package.sh
+    bash -x build_deb_package.sh
 fi
