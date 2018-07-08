e69e3cf121e071c4f028a9d1e3a236522dcc3a22
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2018-07-08 16:21:27 +0900

    test

---
+++ build_win_package.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set BIN_NAME=sqlitebiter
+
+pip install --upgrade pip
+pip install --upgrade .[build]
+
+echo "----- start build -----"
+pyinstaller cli.py --onefile --name %BIN_NAME% --clean --noconfirm --specpath build
+echo "----- complete build -----"
+
+echo "----- start compress -----"
+set DIST_DIR_NAME=dist
+set BIN_PATH=%DIST_DIR_NAME%/%BIN_NAME%.exe
+set ARCHIVE_PATH=%DIST_DIR_NAME%/%BIN_NAME%_win_x64.zip
+
+echo %ARCHIVE_PATH%
+powershell compress-archive -Force %BIN_PATH% %ARCHIVE_PATH%
+echo "----- complete compress -----"
+
+pause
