64089d9cacd1105600ef6ce7f36bd060be5bc131
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-03-29 20:40:30 +0900

    test

--- build_win_package.bat
+++ build_win_package.bat
@@ -5,7 +5,7 @@ setlocal
 set BIN_NAME=sqlitebiter
 
 echo "----- start build -----"
-%PYTHON%/Scripts/pyinstaller.exe cli.py --onefile --name %BIN_NAME% --clean --noconfirm --additional-hooks-dir=scripts/hooks/hook-jsonschema.py --specpath build
+%PYTHON%/Scripts/pyinstaller.exe cli.py --onefile --name %BIN_NAME% --clean --noconfirm --additional-hooks-dir=scripts/hooks --specpath build
 echo "----- complete build -----"
 
 echo "----- start compress -----"
