2f8dbdc770994f2405e7ba4fe0c64c2d33620806
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-08-09 11:08:20 +0900

    test

--- .travis/run_test.sh
+++ .travis/run_test.sh
@@ -3,6 +3,9 @@
 pyver=$(python -c "from __future__ import print_function; import sys; print('{}{}'.format(*sys.version_info[0:2]))")
 echo $pyver
 
+echo $CI
+echo $TRAVIS
+
 if [ "$TOXENV" != "build" ] ; then
     tox
 fi
