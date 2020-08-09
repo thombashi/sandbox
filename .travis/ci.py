4f548d0423befbfca043dede9313d7b71c3be100
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-08-09 11:41:03 +0900

    test

--- .travis/ci.py
+++ .travis/ci.py
@@ -1,11 +1,19 @@
+#!/usr/bin/env python3
+
 import os
 
-CI = os.environ.get("CI")
-print(CI)
-if CI:
-    print(CI == "true", CI.strip() == "true")
 
-TRAVIS = os.environ.get("TRAVIS")
-print(TRAVIS)
-if TRAVIS:
-    print(TRAVIS == "true", TRAVIS.strip() == "true")
+def main():
+    CI = os.environ.get("CI")
+    print(CI)
+    if CI:
+        print(CI == "true", CI.strip() == "true")
+
+    TRAVIS = os.environ.get("TRAVIS")
+    print(TRAVIS)
+    if TRAVIS:
+        print(TRAVIS == "true", TRAVIS.strip() == "true")
+
+
+if __name__ == '__main__':
+    main()
